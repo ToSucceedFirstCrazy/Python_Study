edad74e8ec31b0582025e3296ad822a5a4965fa0
ToSucceedFirstCrazy <43458263+ToSucceedFirstCrazy@users.noreply.github.com>
2019-03-13 22:59:22 +0800

    测试

---
+++ 解释器.py
@@ -0,0 +1,6 @@
+# @Time    : 2019/3/13 22:57
+# @Author  : ToSucceedFirstCrazy 
+# @Email   : 179942072@qq.com
+# @Wish    : Good Good Study Day Day Up
+# @Blog    : https://github.com/ToSucceedFirstCrazy/Python_Study
+print("asdf")
