c15bc876ede2edec09a7fe54266b3e1ea568cc0a
ToSucceedFirstCrazy <179942072@qq.com>
2019-03-15 11:09:05 +0800

    Update 京东购物网.py

--- 京东购物网.py
+++ 京东购物网.py
@@ -3,29 +3,70 @@
 # @Email   : 179942072@qq.com
 # @Wish    : Good Good Study Day Day Up
 # @Blog    : https://github.com/ToSucceedFirstCrazy/Python_Study
-username = "Bob"
-password = "123"
+user = "Bob"
+passwd = "123"
 times = 0
-flag = False
+login_status = False
+status = True
 
 
-def outer(f):
+def login(f):
     def inner():
-        if global flag:
-            f()
-        else:
-            if times >= 3:
-                print("your count is lock")
-            else:
-                name = input("please input your username :")
-                word = input("please input your password :")
-                if name == username and word == password:
-                    print(" Welcom to shopping")
-                    global flag = True
+        global times
+        global login_status
+        if not login_status:
+            if times < 3:
+                username = input("please input username:")
+                password = input("please input password:")
+                if user == username and passwd == password:
+                    print("welcome......")
+                    f()
+                    login_status = True
                 else:
-                    print("username or password is wrong")
-                    global times = global times + 1
+                    print("username or password is wrong!!!")
+                    times += 1
+            else:
+                print("your count is locked")
 
+        else:
+            f()
+    return inner
+
+
+@login
+def book():
+    print("book")
+
+
+@login
+def shoes():
+    print("shoes")
+
+
+@login
+def food():
+    print("food")
+
+
+while status:
+    choice = int(input("________________________\n"
+          "________1.book__________\n"
+          "________2.shoes_________\n"
+          "________3.food__________\n"
+          "________4.exit__________\n"
+          "________________________\n"
+          "please choice your list:\n"))
+    if choice == 1:
+        book()
+    elif choice == 2:
+        shoes()
+    elif choice == 3:
+        food()
+    elif choice == 4:
+        status = False
+        print("bay~~~")
+    else:
+        print("your choice is wrong!!!")
 
 
 
