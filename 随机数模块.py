585e9fcdeb901ac2bcf62191409fb6633592e149
ToSucceedFirstCrazy <43458263+ToSucceedFirstCrazy@users.noreply.github.com>
2019-03-25 22:22:54 +0800

    Create 随机数模块.py

---
+++ 随机数模块.py
@@ -0,0 +1,13 @@
+# @Time    : 2019/3/25 20:48
+# @Author  : ToSucceedFirstCrazy 
+# @Email   : 179942072@qq.com
+# @Wish    : Good Good Study Day Day Up
+# @Blog    : https://github.com/ToSucceedFirstCrazy/Python_Study
+import random
+print(random.random())
+print(random.randint(1, 3))
+print(random.choice("hello"))
+print(random.choice(["123", 4, [1, 4]]))
+print(random.sample(["123", 4, [1, 4]], 2)) # 随机选择2个
+print(random.randrange(1, 3))
+
