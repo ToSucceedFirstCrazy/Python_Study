b55bf5a83e10e88fa2be147d94f31fa280dbe3c6
ToSucceedFirstCrazy <43458263+ToSucceedFirstCrazy@users.noreply.github.com>
2019-03-25 22:22:51 +0800

    Create os模块.py

---
+++ os模块.py
@@ -0,0 +1,10 @@
+# @Time    : 2019/3/25 22:12
+# @Author  : ToSucceedFirstCrazy 
+# @Email   : 179942072@qq.com
+# @Wish    : Good Good Study Day Day Up
+# @Blog    : https://github.com/ToSucceedFirstCrazy/Python_Study
+import os
+
+print(help(os))
+print(os.getcwd())
+print(os.chdir())
